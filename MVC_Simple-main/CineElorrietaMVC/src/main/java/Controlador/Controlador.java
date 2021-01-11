72bac5f9ba91996aa41bcb181bc75dd0d43acba4
unaibrrgn <75972112+unaibrrgn@users.noreply.github.com>
2021-01-11 09:15:31 +0100

    Add files via upload

---
+++ MVC_Simple-main/CineElorrietaMVC/src/main/java/Controlador/Controlador.java
@@ -0,0 +1,30 @@
+package Controlador;
+
+import Modelo.Modelo;
+import Vista.Vista;
+
+public class Controlador {
+
+	private Modelo modelo;
+	private Vista vista;
+	private ControladorPanelBienvenida controladorPanelBienvenida;
+	private ControladorPanelGeneros controladorPanelGeneros;
+	
+	public Controlador(Modelo modelo, Vista vista) {
+		this.modelo = modelo;
+		this.vista = vista;
+		this.controladorPanelBienvenida = new ControladorPanelBienvenida(this.modelo, this.vista, this);
+		this.controladorPanelGeneros = new ControladorPanelGeneros(this.modelo, this.vista, this);
+		this.navegarPanelBienvenida();
+	}
+	
+	public void navegarPanelBienvenida() {
+		System.out.println("Navegar panel Bienvenida");
+		this.controladorPanelBienvenida.mostrarPanelBienvenida();
+	}
+	
+	public void navegarPanelGeneros() {
+		System.out.println("Navegar panel Generos");
+		this.controladorPanelGeneros.mostrarPanelGeneros();
+	}
+}
---
+++ MVC_Simple-main/CineElorrietaMVC/src/main/java/Vista/PanelGeneros.java
@@ -0,0 +1,47 @@
+package Vista;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import Controlador.ControladorPanelGeneros;
+
+@SuppressWarnings("serial")
+public class PanelGeneros extends JPanel {
+
+	private JButton btnVolver;
+	private JLabel lblGeneros;
+	private ControladorPanelGeneros controladorPanelGeneros;
+	
+	public PanelGeneros(ControladorPanelGeneros controladorPanelGeneros)
+	{
+		this.controladorPanelGeneros = controladorPanelGeneros;
+		
+		setLayout(null);
+		
+		lblGeneros = new JLabel("Panel Generos");
+		lblGeneros.setBounds(58, 35, 115, 14);
+		add(lblGeneros);
+		
+		btnVolver = new JButton("Volver");
+		btnVolver.setBounds(58, 62, 89, 23);
+		add(btnVolver);
+		
+		initializeEvents();
+	}
+	
+	private void initializeEvents() {
+		this.btnVolver.addActionListener(listenerBotonVolver(this.controladorPanelGeneros));
+	}
+	
+	private ActionListener listenerBotonVolver(ControladorPanelGeneros controladorPanelGeneros) {
+		return new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Ejecutando evento Boton Volver");
+				controladorPanelGeneros.accionadoBottonVolverPanelGeneros();
+			}
+		};
+	}
+}
